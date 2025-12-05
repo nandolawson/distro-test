bf5b14ae8ad681840575c5a4ec3e660039d9392b
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2025-12-05 14:19:56 +0100

    Update flatpak_installer.sh

--- build_files/scripts/flatpak_installer.sh
+++ build_files/scripts/flatpak_installer.sh
@@ -3,6 +3,8 @@ set -ouex pipefail
 
 source /ctx/config
 
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 IFS=',' read -r -a apps <<< "$flatpaks"
 
 for app in "${apps[@]}"; do
