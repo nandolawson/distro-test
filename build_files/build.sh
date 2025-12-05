9cd52dbdeb60d1a1ca741ded55c2acb54ad6db65
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2025-12-05 14:26:41 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,8 +8,3 @@ if [[ "$debloater" == "true" ]]; then
     echo ">> Running debloater script"
     bash /ctx/scripts/debloater.sh
 fi
-
-if [[ -n "${flatpaks:-}" ]]; then
-    echo ">> Running Flatpak installer"
-    bash /ctx/scripts/flatpak_installer.sh
-fi
